ee3d3c29ead7f6a19dc8ccca0faf99fc95bc3406
Maskedheroe <296994179@qq.com>
2019-04-05 23:40:06 +0800

    pr 学习到处理padding的方法

--- app/src/main/java/com/example/pieBitMap/PieView.kt
+++ app/src/main/java/com/example/pieBitMap/PieView.kt
@@ -2,6 +2,7 @@ package com.example.pieBitMap
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 
@@ -21,14 +22,23 @@ class PieEntry {
     //扇区总角度
     private var sweepAngle: Float? = null
 }
-class MyPieView:View{
 
-    constructor(context: Context):super(context)
-    constructor(context: Context,attr:AttributeSet?):super(context,attr){
+class MyPieView : View {
+
+    private var mTotalWidth: Int? = null
+
+    private var mTotalHeight: Int? = null
+
+    private var mRadius: Int? = null
+
+    private lateinit var mRectF: RectF
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attr: AttributeSet?) : super(context, attr) {
         init()
     }
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     private fun init() {
 
@@ -40,5 +50,28 @@ class MyPieView:View{
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+
+        canvas!!.translate(mTotalWidth!! / 2.toFloat(), mTotalHeight!! / 2.toFloat())
     }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        //获取实际的View宽高
+
+        //!!!!!处理padding的方法
+        mTotalWidth = w - paddingStart - paddingEnd
+        mTotalHeight = h - paddingTop - paddingBottom
+
+        initRectF()
+    }
+
+
+    private fun initRectF() {
+        val shortSideLength = if (mTotalHeight!! < mTotalWidth!!) mTotalHeight else mTotalWidth
+
+        mRadius = shortSideLength!!.shr(2)
+
+        mRectF = RectF(-mRadius!!.toFloat(),-mRadius!!.toFloat(),mRadius!!.toFloat(),mRadius!!.toFloat())
+    }
+
 }
